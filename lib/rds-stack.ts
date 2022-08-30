0019f8453fcd55dbaea0a62971d7305cd246d399
Ushio Shugo <ushio.s@gmail.com>
2022-08-30 23:14:38 +0900

    using merge

--- lib/rds-stack.ts
+++ lib/rds-stack.ts
@@ -1,9 +1,9 @@
-import { aws_ec2,   aws_rds as rds, Stack } from "aws-cdk-lib";
+import { aws_ec2, aws_rds as rds, Stack } from "aws-cdk-lib";
 
 import { Construct } from "constructs";
 import { BaseStackProps } from "./props";
 import { InstanceType } from "aws-cdk-lib/aws-ec2";
-import { DatabaseInstanceEngine } from "aws-cdk-lib/aws-rds";
+import { DatabaseInstanceBase, DatabaseInstanceEngine, DatabaseInstanceProps, DatabaseInstanceSourceProps } from "aws-cdk-lib/aws-rds";
 
 export interface RdsStackProps extends BaseStackProps {
   rdsName: string
@@ -19,31 +19,28 @@ export class RdsStack extends Stack {
   constructor(scope: Construct, id: string, props: RdsStackProps) {
     super(scope, id, props);
 
+
+    const rdsProps: DatabaseInstanceSourceProps = {
+      engine: DatabaseInstanceEngine.postgres({ version: props.postgresVersion }),
+      instanceType: props.instanceType,
+      databaseName: props.rdsName,
+      vpc: props.vpc,
+      vpcSubnets: {
+        subnets: props.vpc.isolatedSubnets
+      },
+      securityGroups: [props.securityGroup]
+    }
+
     // snapshotから復元するかどうか
     if (props.snapshot) {
       new rds.DatabaseInstanceFromSnapshot(this, 'restoreRds', {
-        snapshotIdentifier: props.snapshotIdentifier,
-        engine: rds.DatabaseInstanceEngine.postgres({ version: props.postgresVersion }),
-        databaseName: props.rdsName,
-        instanceType: props.instanceType,
-        vpc: props.vpc,
-        vpcSubnets: {
-          subnets: props.vpc.publicSubnets
-        },
-        securityGroups: [props.securityGroup]
+        ...rdsProps,
+        ...{
+          snapshotIdentifier: props.snapshotIdentifier,
+        }
       })
     } else {
-      new rds.DatabaseInstance(this, 'createRds', {
-        engine: DatabaseInstanceEngine.postgres({version: props.postgresVersion}),
-        instanceType: props.instanceType,
-        databaseName: props.rdsName,
-        vpc: props.vpc,
-        vpcSubnets: {
-          subnets: props.vpc.isolatedSubnets
-        },
-        securityGroups: [props.securityGroup]
-
-      })
+      new rds.DatabaseInstance(this, 'createRds', rdsProps)
     }
   }
 }
