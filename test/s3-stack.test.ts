3014aac7682f3497cf40e65aba044dc1ada68ab9
takahashim <takahashimm@gmail.com>
2022-11-28 18:02:23 +0900

    Add S3Stack snapshot test

---
+++ test/s3-stack.test.ts
@@ -0,0 +1,31 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+
+import { Config, getConfig } from "../lib/config";
+import { NetworkStack } from "../lib/network";
+import { S3Stack } from "../lib/s3-stack";
+
+test('S3Stack Created', () => {
+    const app = new cdk.App();
+
+    const stage = 'staging';
+    const config: Config = getConfig(stage)
+    const serviceName = `decidim`;
+
+    const env = {
+        account: config.aws.accountId,
+        region: config.aws.region
+    }
+
+    const s3stack = new S3Stack(app, `${ stage }${ serviceName }S3Stack`, {
+        stage,
+        env,
+        serviceName
+    })
+
+    const template = Template.fromStack(s3stack);
+
+    // Assert the template matches the snapshot.
+    expect(template.toJSON()).toMatchSnapshot();
+});
+
