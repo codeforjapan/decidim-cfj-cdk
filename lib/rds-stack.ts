4446446ea6050a83ae931949a94db5fd2e707508
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2023-08-21 18:59:02 +0900

    refactor: update postgres version

--- lib/rds-stack.ts
+++ lib/rds-stack.ts
@@ -25,7 +25,7 @@ export class RdsStack extends Stack {
     const config = props.rds;
 
     const rdsProps: DatabaseInstanceSourceProps = {
-      engine: DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_12_11 }),
+      engine: DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_12_14 }),
       instanceType: config.instanceType,
       instanceIdentifier: `${ props.stage }-${ props.serviceName }-postgresql`,
       vpc: props.vpc,
