7ddd77668ddb3cf48cf1bc57d7b9ec41929a99d7
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2025-01-30 20:42:32 +0900

    feat: use 283

--- lib/elasticache-stack.ts
+++ lib/elasticache-stack.ts
@@ -31,7 +31,7 @@ export class ElasticacheStack extends Stack {
       cacheSubnetGroupName: props.ecSubnetGroup.cacheSubnetGroupName,
     }
 
-    if (props.stage === 'prd-v0274') {
+    if (props.stage === 'prd-v0283') {
       this.redis = new elasticache.CfnReplicationGroup(this, 'prdElasticache', {
         ...elastiCacheProps,
         ...{
