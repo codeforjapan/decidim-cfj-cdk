67b8de2001f55f3adf010189be526b3696da56a1
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2025-06-25 17:15:15 +0900

    feat: update packages & rds version

--- lib/rds-stack.ts
+++ lib/rds-stack.ts
@@ -25,7 +25,7 @@ export class RdsStack extends Stack {
     const config = props.rds;
 
     const rdsProps: DatabaseInstanceSourceProps = {
-      engine: DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_14_12 }),
+      engine: DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_14_17 }),
       instanceType: config.instanceType,
       instanceIdentifier: `${ props.stage }-${ props.serviceName }-postgresql`,
       vpc: props.vpc,
